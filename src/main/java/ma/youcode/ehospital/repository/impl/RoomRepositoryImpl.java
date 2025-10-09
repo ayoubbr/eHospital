976644fea5931dbb417ba562dfd181eddb5dadd2
unknown <ayoub1benmansour@gmail.com>
2025-10-09 15:39:36 +0100

    Room repository

---
+++ src/main/java/ma/youcode/ehospital/repository/impl/RoomRepositoryImpl.java
@@ -0,0 +1,54 @@
+package ma.youcode.ehospital.repository.impl;
+
+import jakarta.persistence.EntityManager;
+import ma.youcode.ehospital.model.Room;
+import ma.youcode.ehospital.repository.IRoomRepository;
+import ma.youcode.ehospital.util.JPAUtil;
+
+import java.util.List;
+
+public class RoomRepositoryImpl implements IRoomRepository {
+    @Override
+    public Room save(Room room) {
+        EntityManager em = JPAUtil.getEntityManager();
+        em.getTransaction().begin();
+        em.persist(room);
+        em.getTransaction().commit();
+        return room;
+    }
+
+    @Override
+    public Room update(Room room) {
+        EntityManager em = JPAUtil.getEntityManager();
+        em.getTransaction().begin();
+        em.merge(room);
+        em.getTransaction().commit();
+        return room;
+    }
+
+    @Override
+    public void delete(int id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        Room room = em.find(Room.class, id);
+        em.getTransaction().begin();
+        em.remove(room);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public List<Room> findAll() {
+        EntityManager em = JPAUtil.getEntityManager();
+        List<Room> rooms = em.createQuery("from Room", Room.class).getResultList();
+        em.close();
+        return rooms;
+    }
+
+    @Override
+    public Room findById(int id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        Room room = em.find(Room.class, id);
+        em.close();
+        return room;
+    }
+}
